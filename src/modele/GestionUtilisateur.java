276c0618b6a0abcddaa38beb166b47ce4eed86e0
NattyRoot <felix.soignon@gmail.com>
2018-02-05 12:17:08 +0100

    felix : Servlet connexion

--- src/modele/GestionUtilisateur.java
+++ src/modele/GestionUtilisateur.java
@@ -1,7 +1,14 @@
 package modele;
 
+import java.util.List;
+
+import beans.Epreuve;
+import beans.Utilisateur;
+
 public interface GestionUtilisateur {
 
 	public Utilisateur connecterUtilisateur(String email, String password);
+	
+	public List<Epreuve> getListEpreuve();
 	
 }

2b5d0452e020f80cd88f1ca92462414d05291baa
Majimo <pierre.fervel@gmail.com>
2018-02-05 11:46:27 +0100

    Pierre: beans Utilisateur, Promotion, Epreuve

---
+++ src/beans/Utilisateur.java
@@ -0,0 +1,74 @@
+package beans;
+
+public class Utilisateur {
+
+	private int idUtilisateur;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String password;
+	private String codePromo;
+	
+	public Utilisateur(){
+		
+	}
+
+	public Utilisateur(int idUtilisateur, String nom, String prenom, String email, String password, String codePromo) {
+		super();
+		this.idUtilisateur = idUtilisateur;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.password = password;
+		this.codePromo = codePromo;
+	}
+
+	public int getIdUtilisateur() {
+		return idUtilisateur;
+	}
+
+	public void setIdUtilisateur(int idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCodePromo() {
+		return codePromo;
+	}
+
+	public void setCodePromo(String codePromo) {
+		this.codePromo = codePromo;
+	}
+	
+}
